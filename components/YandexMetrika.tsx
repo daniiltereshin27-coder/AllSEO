4637e790414e43b676cbecb681d765ad8ce6b833
user <user@MacBook-Pro--Daniil.local>
2026-07-06 11:48:36 +0300

    Align Yandex Metrika snippet with official code

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -15,26 +15,19 @@ export function YandexMetrika() {
             for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
           })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${counterId}', 'ym');
-          ym(${counterId}, 'init', {
-            ssr: true,
-            webvisor: true,
-            clickmap: true,
-            ecommerce: "dataLayer",
-            referrer: document.referrer,
-            url: location.href,
-            accurateTrackBounce: true,
-            trackLinks: true
-          });
+          ym(${counterId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
         `,
         }}
       />
       <noscript>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={`https://mc.yandex.ru/watch/${counterId}`}
-          style={{ position: "absolute", left: "-9999px" }}
-          alt=""
-        />
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={`https://mc.yandex.ru/watch/${counterId}`}
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
       </noscript>
     </>
   );
